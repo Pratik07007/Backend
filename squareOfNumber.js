4cc501b7ab6c0b3ba849b76a5e85375ad9693cf4
Pratik07007 <s.dhimal006@gmail.com>
2024-04-13 21:15:28 +0545

    Testing local server

---
+++ squareOfNumber.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+//using params
+const square = (num) => {
+  return num * num;
+};
+
+app.get("/:num", (req, res) => {
+  const num = req.params.num; //accesing headers
+  console.log(req.headers);
+  const result = square(num);
+  res.json({
+    result,
+  });
+});
+
+
+
+app.listen(3000, () => {});
