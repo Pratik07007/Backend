095c2fa33c0d4bc74cc4956b17fd0a2f2746326e
Pratik07007 <s.dhimal006@gmail.com>
2024-04-15 09:58:50 +0545

    loginValidiation

--- advancePasswordValidiationUsingMiddleWares.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-const middleWare=()=>{
-    
-}
-
-
-app.listen(3000)
---
+++ loginValudiation.js
@@ -0,0 +1,34 @@
+const zod = require("zod");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const loginPayloadValidiation = (payload) => {
+  const loginSchema = zod.object({
+    email: zod.string().email(), //this direclty validiated the email checking varipus factors to fit he email class defined in the core logic
+    password: zod
+      .string()
+      .min(8)
+      .max(32)
+      .regex(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}|:"<>?]).{8,32}$/, {
+        message:
+          'Password must contain at least one uppercase letter and one special character (!@#$%^&*()_+{}|:"<>?)',
+      }),
+  });
+  const response = loginSchema.safeParse(payload);
+  return response.error || response;
+};
+
+app.post("/login", (req, res) => {
+  const loginPayload = req.body;
+  const serverResposne = loginPayloadValidiation(loginPayload);
+  res.json(serverResposne);
+});
+
+app.listen(3000);
+
+const payload = {
+  email: "s.dhimal006@gmail.com",
+  password: "Access@123",
+};
